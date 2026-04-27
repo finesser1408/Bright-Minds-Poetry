f9ba0ef7b8c05e05e4d43316c381068cbb248587
finesser1408 <166210988+finesser1408@users.noreply.github.com>
2026-04-27 08:52:07 +0200

    switched from sqlite to postgresql

--- backend/brightminds/settings.py
+++ backend/brightminds/settings.py
@@ -114,8 +114,12 @@ WSGI_APPLICATION = 'brightminds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BrightMinds',
+        'USER': 'postgres',
+        'PASSWORD': 'Allan06!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
